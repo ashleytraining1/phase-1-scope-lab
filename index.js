8757d3e2b54a8259c3bc60bc00b94cb4bb08f5d9
ashley <ashley.oketo@student.moringaschool.com>
2022-08-17 10:14:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+     customerName = 'BOB';
+    return 'BOB';
+}
+
+
+function setBestCustomer() {
+
+    bestCustomer = 'not bob';
+    
+    
+
+}function overwriteBestCustomer(){
+     bestCustomer = 'maybe bob';
+    return "bestCustomer"
+}
+
+const leastFavoriteCustomer = 'ashley';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'monicah';
+return 'monicah';
+}
+
+
